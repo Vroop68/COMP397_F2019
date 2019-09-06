cf66cb2778adc3f48eb777edbc37f08a5198aaa9
John Knoop <john.r.knoop@gmail.com>
2019-09-06 15:02:56 -0400

    added createjs

--- Scripts/Core/game.js
+++ Scripts/Core/game.js
@@ -1,8 +1,30 @@
 // Immediate invoked anonymous function
 (function () {
+    //Global Game Variables
+    var canvas = document.getElementById("canvas");
+    var stage;
+    var helloLabel;
     function Init() {
-        console.log("Initialisation Start");
-        var x = 10;
+        console.log("Initialization Start");
+        Start();
+    }
+    function Start() {
+        console.log("Starting Application...");
+        //Initialize createjs
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
+        Main();
+    }
+    function Update() {
+        stage.update();
+    }
+    function Main() {
+        console.log("Game Start...");
+        helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+        stage.addChild(helloLabel); //Addes label to stage
     }
     window.onload = Init;
 })();
--- Scripts/Core/game.ts
+++ Scripts/Core/game.ts
@@ -1,10 +1,37 @@
 // Immediate invoked anonymous function
 
 (function() {
-  function Init() {
-    console.log("Initialisation Start");
+  //Global Game Variables
 
-    let x: number = 10;
+  let canvas = document.getElementById("canvas");
+  let stage: createjs.Stage;
+  let helloLabel: createjs.Text;
+
+  function Init() {
+    console.log("Initialization Start");
+    Start();
+  }
+
+  function Start() {
+    console.log("Starting Application...");
+    //Initialize createjs
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.on("tick", Update);
+    Main();
+  }
+
+  function Update() {
+    stage.update();
+  }
+
+  function Main() {
+    console.log("Game Start...");
+    helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+    helloLabel.x = 100;
+    helloLabel.y = 100;
+
+    stage.addChild(helloLabel); //Addes label to stage
   }
 
   window.onload = Init;
